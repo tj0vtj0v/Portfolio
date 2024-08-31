862b4b5197982af4d947e9ed1940c287c4a20587
Tjorven Burdorf (TG-650) <Tjorven.Burdorf@bmw.de>
2024-08-31 12:51:11 +0200

    Initial setup done at work

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./features/home/home.component";
+import {AboutComponent} from "./features/about/about.component";
+import {PrivateComponent} from "./features/private/private.component";
+import {PageNotFoundComponent} from "./features/page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'private', component: PrivateComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
---
+++ src/app/features/private/private.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-private',
+  standalone: true,
+  imports: [],
+  templateUrl: './private.component.html',
+  styleUrl: './private.component.css'
+})
+export class PrivateComponent {
+
+}
