b5ce5a9a3265573586db40824e332ddcea4ca85d
tjorven <burdorftjorven@gmail.com>
2024-12-10 12:54:17 +0100

    make authentication work

--- src/app/features/authentication/authentication.component.ts
+++ src/app/features/authentication/authentication.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthenticationService} from '../../shared/authentication.service';
 import {FormsModule} from '@angular/forms';
-import {HttpResponse} from '@angular/common/http';
 
 @Component({
     selector: 'app-authentication',
@@ -23,11 +22,6 @@ export class AuthenticationComponent {
             return;
         }
 
-        this.authenticationService.login(this.username, this.password).subscribe(
-            (response: HttpResponse<any>) => {
-                console.log(response);
-                localStorage.setItem('auth-token', response.body);
-            }
-        )
+        this.authenticationService.login(this.username, this.password).subscribe()
     }
 }
--- src/app/shared/authentication.service.ts
+++ src/app/shared/authentication.service.ts
@@ -1,26 +1,44 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import {catchError, Observable} from 'rxjs';
+import {catchError, map} from 'rxjs';
+
+interface AuthResponse {
+    access_token: string;
+    token_type: string;
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthenticationService {
-    private loginUrl = 'http://raspi:4053/login';
+    private loginUrl = 'http://80.209.200.73:4053/login';
+
 
     constructor(private http: HttpClient) {
     }
 
-    login(username: string, password: string): Observable<any> {
+    login(username: string, password: string) {
         const body = new HttpParams().set('username', username).set('password', password);
         const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
-        return this.http.post(this.loginUrl, body.toString(), {headers}).pipe(
-            catchError((error) => {
-                if (error.status === 401) {
-                    alert(error.error.detail);
+        return this.http.post<AuthResponse>(this.loginUrl, body.toString(), {headers}).pipe(
+            map((response: AuthResponse) => {
+                console.log(response);
+                if (response.access_token) {
+                    localStorage.setItem('auth-token', response.access_token);
                 }
-                return '';
+            }),
+            catchError((error) => {
+                alert(error?.error?.detail);
+                throw error
             })
         )
+    }
+
+    logout(): void {
+        localStorage.removeItem('auth-token');
+    }
+
+    is_logged_in(): boolean {
+        return localStorage.getItem('auth-token') !== undefined;
     }
 }
