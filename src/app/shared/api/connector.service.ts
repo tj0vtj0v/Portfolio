c77e7972954c35299d53de9e7039d26b7c96f516
tjorven - DESKTOP-O11JQUU <burdorftjorven@gmail.com>
2025-01-12 14:38:15 +0100

    fix cookie management

--- src/app/shared/api/connector.service.ts
+++ src/app/shared/api/connector.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import {catchError, map, Observable, take, throwError} from 'rxjs';
+import {catchError, EMPTY, firstValueFrom, from, map, Observable, switchMap, take} from 'rxjs';
 import {AuthResponse} from '../datatype/AuthResponse';
 import {Router} from '@angular/router';
 
@@ -9,6 +9,7 @@ import {Router} from '@angular/router';
 })
 export class ConnectorService {
     private url = 'http://80.209.200.73:4053/' // TODO
+    private refreshPromise?: Promise<void>;
 
     constructor(
         private http: HttpClient,
@@ -20,21 +21,10 @@ export class ConnectorService {
         const body = new HttpParams().set('username', username).set('password', password);
         const headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
 
-        return this.http.post<AuthResponse>(`${this.url}login`, body.toString(), {headers}).pipe(
-            take(1),
-            map((response: AuthResponse) => {
-                if (response.access_token) {
-                    localStorage.setItem('token', response.access_token);
-                    localStorage.setItem('token-type', response.token_type);
-                } else {
-                    throw ('Unexpected login response');
-                }
-            })
-        );
-    }
-
-    refresh() {
-        return this.http.post<AuthResponse>(`${this.url}refresh`, null).pipe(
+        return this.http.post<AuthResponse>(
+            `${this.url}login`, body.toString(),
+            {headers, withCredentials: true}
+        ).pipe(
             take(1),
             map((response: AuthResponse) => {
                 if (response.access_token) {
@@ -48,67 +38,124 @@ export class ConnectorService {
     }
 
     add(suffix: string, body: any, recursive: boolean = false): Observable<any> {
-        return this.http.post(`${this.url}${suffix}`, body, {headers: this.buildSendHeader()}).pipe(
+        return this.http.post(
+            `${this.url}${suffix}`, body,
+            {headers: this.buildSendHeader()}
+        ).pipe(
             catchError(error => {
                 if (error.error?.detail === 'Authorisation token expired') {
                     if (!recursive) {
-                        this.refresh()
-                        return this.add(suffix, body, true)
-                    } else {
-                        this.logout()
+                        return from(this.refresh_handler()).pipe(
+                            switchMap(() => this.add(suffix, body, true)),
+                            catchError(() => {
+                                return EMPTY;
+                            })
+                        )
                     }
                 }
-                return throwError(error);
+                throw error;
             })
         );
     }
 
     get(suffix: string, recursive: boolean = false): Observable<any> {
-        return this.http.get(`${this.url}${suffix}`, {headers: this.buildRequestHeader()}).pipe(
+        return this.http.get(
+            `${this.url}${suffix}`,
+            {headers: this.buildRequestHeader()}
+        ).pipe(
             catchError(error => {
                 if (error.error?.detail === 'Authorisation token expired') {
                     if (!recursive) {
-                        this.refresh()
-                        return this.get(suffix, true)
-                    } else {
-                        this.logout()
+                        return from(this.refresh_handler()).pipe(
+                            switchMap(() => this.get(suffix, true)),
+                            catchError(() => {
+                                return EMPTY;
+                            })
+                        )
                     }
                 }
-                return throwError(error);
+                throw error;
             })
         );
     }
 
     update(suffix: string, body: any, recursive: boolean = false): Observable<any> {
-        return this.http.patch(`${this.url}${suffix}`, body, {headers: this.buildSendHeader()}).pipe(
+        return this.http.patch(
+            `${this.url}${suffix}`, body,
+            {headers: this.buildSendHeader()}
+        ).pipe(
             catchError(error => {
                 if (error.error?.detail === 'Authorisation token expired') {
                     if (!recursive) {
-                        this.refresh()
-                        this.update(suffix, body, true)
-                    } else {
-                        this.logout()
+                        return from(this.refresh_handler()).pipe(
+                            switchMap(() => this.update(suffix, body, true)),
+                            catchError(() => {
+                                return EMPTY;
+                            })
+                        )
                     }
                 }
-                return throwError(error);
+                throw error;
             })
         );
     }
 
     delete(suffix: string, recursive: boolean = false): Observable<any> {
-        return this.http.delete(`${this.url}${suffix}`, {headers: this.buildDeleteHeader()}).pipe(
+        return this.http.delete(
+            `${this.url}${suffix}`,
+            {headers: this.buildDeleteHeader()}
+        ).pipe(
             catchError(error => {
                 if (error.error?.detail === 'Authorisation token expired') {
                     if (!recursive) {
-                        this.refresh()
-                        this.delete(suffix, true)
-                    } else {
-                        this.logout()
+                        return from(this.refresh_handler()).pipe(
+                            switchMap(() => this.delete(suffix, true)),
+                            catchError(() => {
+                                return EMPTY;
+                            })
+                        )
                     }
                 }
-                return throwError(error);
+                throw error;
             })
         );
+    }
+
+    private refresh(): Promise<void> {
+        return firstValueFrom(
+            this.http.post<AuthResponse>(
+                `${this.url}login/refresh`, {},
+                {headers: this.buildSendHeader(), withCredentials: true}
+            ).pipe(
+                take(1),
+                map((response: AuthResponse) => {
+                    if (response.access_token) {
+                        localStorage.setItem('token', response.access_token);
+                        localStorage.setItem('token-type', response.token_type);
+                    } else {
+                        throw ('Unexpected refresh response');
+                    }
+                })
+            )
+        )
+    }
+
+    private refresh_handler() {
+        if (this.refreshPromise !== undefined) {
+            return this.refreshPromise!
+        }
+
+        this.refreshPromise = this.refresh()
+            .then(() => {
+                this.refreshPromise = undefined;
+            })
+            .catch((error) => {
+                this.refreshPromise = undefined;
+                this.logout()
+                throw error;
+            })
+
+        return this.refreshPromise
     }
 
     private logout() {
