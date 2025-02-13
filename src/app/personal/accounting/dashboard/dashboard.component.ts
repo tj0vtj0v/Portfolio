9c0eba6b75847dda7caefd2ecb5040427af4b2a8
tjorven - DESKTOP-O11JQUU <burdorftjorven@gmail.com>
2025-02-13 13:44:51 +0100

    markup diagrams

--- src/app/personal/accounting/dashboard/dashboard.component.ts
+++ src/app/personal/accounting/dashboard/dashboard.component.ts
@@ -40,25 +40,28 @@ export class DashboardComponent implements OnInit {
     //original data
     protected accounts: Account[] = [];
     protected categories: Category[] = [];
+    private expenses: Expense[] = [];
+    private incomes: Income[] = [];
+    private transfers: Transfer[] = [];
+
     //filter
     protected selectedAccounts: Account[] = [];
     protected selectedCategories: Category[] = [];
     protected startDate?: string;
     protected endDate?: string;
+    private histories: Map<string, BalanceHistory[]> = new Map();
+
     //visual data
     protected filteredHistories: Map<string, BalanceHistory[]> = new Map();
     protected filteredExpenses: Expense[] = [];
     protected categoryExpenseMap: Map<string, number> = new Map();
     protected filteredIncomes: Income[] = [];
     protected filteredTransfers: Transfer[] = [];
+
     //visuals
     protected balance_chart: EChartsCoreOption = {};
     protected category_expense_chart: EChartsCoreOption = {};
     protected history_chart: EChartsCoreOption = {};
-    private histories: Map<string, BalanceHistory[]> = new Map();
-    private expenses: Expense[] = [];
-    private incomes: Income[] = [];
-    private transfers: Transfer[] = [];
 
     constructor(
         private accountingService: AccountingService
@@ -100,24 +103,26 @@ export class DashboardComponent implements OnInit {
         });
     }
 
-    build_balance_chart(): void {
+    private build_balance_chart(): void {
+        const totalBalance = this.accounts.reduce((sum, account) => sum + account.balance, 0);
         const refinedAccounts = this.accounts.map(account => (
             {
                 name: account.name,
-                value: account.balance,
+                value: account.balance
             }
         ));
 
         this.balance_chart = {
             title: {
-                text: 'Account Liquidity',
+                text: `Account Liquidity - Total: ${totalBalance}€`,
                 left: 'center',
             },
             tooltip: {
                 trigger: 'item',
                 formatter: (params: any) => {
+                    const percentage = Math.round(params.percent);
                     const value = parseFloat(params.value).toFixed(2);
-                    return `${params.name}: ${value}€`;
+                    return `${params.name}: ${value}€<br>${percentage}%`;
                 },
             },
             legend: {
@@ -143,33 +148,110 @@ export class DashboardComponent implements OnInit {
         };
     }
 
-    build_history_chart(): void {
-        const dates = Array.from(new Set(
-            [...this.filteredHistories.values()].flatMap(history => history.map(entry => entry.date))
-        )).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+    protected balanceLegendListener(ec: any) {
+        ec.on('legendselectchanged', (params: any) => {
+            const selectedNames = Object.keys(params.selected)
+                .filter(accountName => params.selected[accountName]);
 
-        const refinedHistories = Array.from(this.filteredHistories).map(([accountName, history]) => {
-            const historyMap = new Map(history.map(entry => [entry.date, entry.balance]));
+            this.selectedAccounts = this.accounts.filter(account =>
+                selectedNames.includes(account.name)
+            );
 
-            let lastBalance: number;
+            this.filterData();
+        });
+    }
 
-            const data = dates.map(date => {
-                if (historyMap.has(date)) {
-                    lastBalance = historyMap.get(date)!;
+    private build_category_expense_chart(): void {
+        const totalExpenses = Array.from(this.categoryExpenseMap.values()).reduce((sum, expense) => sum + expense, 0);
+        const refinedCategories = Array.from(this.categoryExpenseMap.entries()).map(entry => (
+            {
+                name: entry[0],
+                value: entry[1]
+            }
+        ));
+
+        this.category_expense_chart = {
+            title: {
+                text: `Expenses by Category - Total: ${totalExpenses.toFixed(2)}€`,
+                left: 'center',
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: (params: any) => {
+                    const percentage = parseFloat(params.percent).toFixed(1);
+                    const value = parseFloat(params.value).toFixed(2);
+                    return `${params.name}: ${value}€<br>${percentage}%`;
+                },
+            },
+            legend: {
+                orient: 'vertical',
+                left: 'left',
+                selectedMode: 'multiple',
+            },
+            series: [
+                {
+                    name: 'Category Expenses',
+                    type: 'pie',
+                    radius: '50%',
+                    data: refinedCategories,
+                    emphasis: {
+                        itemStyle: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)',
+                        },
+                    },
+                },
+            ],
+        };
+    }
+
+    protected categoryLegendListener(ec: any) {
+        ec.on('legendselectchanged', (params: any) => {
+            const selectedNames = Object.keys(params.selected)
+                .filter(categoryName => params.selected[categoryName]);
+
+            this.selectedCategories = this.categories.filter(category =>
+                selectedNames.includes(category.name)
+            );
+
+            this.filterData();
+        });
+    }
+
+    private build_history_chart(): void {
+        const dates = Array.from(new Set([...this.filteredHistories.values()]
+            .flatMap(history => history.map(entry => entry.date))
+            .filter(date => !this.startDate || date >= this.startDate))
+        ).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+        const refinedHistories = Array.from(this.filteredHistories)
+            .map(([accountName, history]) => {
+                const historyMap = new Map(history.map(entry => [entry.date, entry.balance]));
+
+                let balance: number;
+                if (!this.startDate && new Date(historyMap.entries().next().value![0]) > new Date("2000-01-01")) {
+                    balance = historyMap.entries().next().value![1];
                 }
 
-                return {date, lastBalance};
+                const data = dates.map(date => {
+                    if (historyMap.has(date) && this.startDate ? this.startDate <= date : true) {
+                        balance = historyMap.get(date)!;
+                    }
+
+                    return {date, balance};
+                });
+
+                return {
+                    name: accountName,
+                    type: 'line',
+                    showSymbol: false,
+                    smooth: true,
+                    data: data.map(entry => entry.balance)
+                };
             });
 
-            return {
-                name: accountName,
-                type: 'line',
-                showSymbol: false,
-                smooth: true,
-                data: data.map(entry => entry.lastBalance)
-            };
-        });
-        console.log(refinedHistories);
+        console.log(refinedHistories)
 
         this.history_chart = {
             title: {
@@ -198,51 +280,8 @@ export class DashboardComponent implements OnInit {
         };
     }
 
-    build_category_expense_chart(): void {
-        const refinedCategories = Array.from(this.categoryExpenseMap.entries()).map(entry => (
-            {
-                name: entry[0],
-                value: entry[1]
-            }
-        ));
-
-        this.category_expense_chart = {
-            title: {
-                text: 'Expenses by Category',
-                left: 'center',
-            },
-            tooltip: {
-                trigger: 'item',
-                formatter: (params: any) => {
-                    const value = parseFloat(params.value).toFixed(2);
-                    return `${params.name}: ${value}€`;
-                },
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left',
-                selectedMode: 'multiple',
-            },
-            series: [
-                {
-                    name: 'Category Expenses',
-                    type: 'pie',
-                    radius: '50%',
-                    data: refinedCategories,
-                    emphasis: {
-                        itemStyle: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)',
-                        },
-                    },
-                },
-            ],
-        };
-    }
-
     logx() {
-        console.log(this.selectedCategories);
+        console.log(this.selectedAccounts);
     }
 
     protected update(): void {
@@ -251,43 +290,23 @@ export class DashboardComponent implements OnInit {
         this.build_category_expense_chart();
     }
 
-    protected balanceLegendListener(ec: any) {
-        ec.on('legendselectchanged', (params: any) => {
-            const selectedNames = Object.keys(params.selected)
-                .filter(accountName => params.selected[accountName]);
-
-            this.selectedAccounts = this.accounts.filter(account =>
-                selectedNames.includes(account.name)
-            );
-
-            this.filterData();
-        });
-    }
-
-    protected categoryLegendListener(ec: any) {
-        ec.on('legendselectchanged', (params: any) => {
-            const selectedNames = Object.keys(params.selected)
-                .filter(categoryName => params.selected[categoryName]);
-
-            this.selectedCategories = this.categories.filter(category =>
-                selectedNames.includes(category.name)
-            );
-
-            this.filterData();
-        });
-    }
-
     private filterData(): void {
         this.filteredHistories = new Map();
         this.histories.forEach((history, name) => {
+            let earlier: BalanceHistory | undefined;
             const filteredHistory = history.filter((entry: BalanceHistory) => {
                 const isAfterStart = this.startDate ? entry.date >= this.startDate : true;
                 const isBeforeEnd = this.endDate ? entry.date <= this.endDate : true;
+                if (!isAfterStart) {
+                    earlier = entry;
+                }
                 return isAfterStart && isBeforeEnd;
             });
 
             const isInAccounts = this.selectedAccounts.some(account => account.name === name)
-            if (filteredHistory.length > 0 && isInAccounts) {
+            if (filteredHistory.length > 0 && isInAccounts && earlier) {
+                this.filteredHistories.set(name, [earlier, ...filteredHistory]);
+            } else {
                 this.filteredHistories.set(name, filteredHistory);
             }
         })
